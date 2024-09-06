a953c3762853055feb3a7fdcd7e6204e024c8314
SriHarshaBS001 <SriHarshaBS009@gmail.com>
2024-09-06 15:19:40 -0700

    Update image used for test pod

--- test/kubernetes/tools.go
+++ test/kubernetes/tools.go
@@ -84,7 +84,7 @@ func DoIntegrationTests(t *testing.T, testCases []test.Case, namespace string) {
 
 // StartClientPod starts a dns client pod in the namespace
 func StartClientPod(namespace string) error {
-	_, err := Kubectl("-n " + namespace + " run " + clientName + " --image=infoblox/dnstools --restart=Never -- -c 'while [ 1 ]; do sleep 100; done'")
+	_, err := Kubectl("-n " + namespace + " run " + clientName + " --image=registry.k8s.io/e2e-test-images/jessie-dnsutils:1.3 --restart=Never -- /bin/sh -c 'while [ 1 ]; do sleep 100; done'")
 	if err != nil {
 		// ignore error (pod already running)
 		return nil
